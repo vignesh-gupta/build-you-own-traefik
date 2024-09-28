901f099921475df02614aaddebf7d69e849db603
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-09-28 20:23:36 +0530

    feat: add docker event listener for new container + reverse proxy from mail service

--- index.js
+++ index.js
@@ -2,9 +2,95 @@
 
 const Docker = require("dockerode");
 const express = require("express");
+const http = require("http");
+const httpProxy = require("http-proxy");
+
+const db = new Map();
+const proxy = httpProxy.createProxy({});
 
 const docker = new Docker();
 
+// Docker Events
+docker.getEvents((err, stream) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  stream.on("data", async (data) => {
+    const event = JSON.parse(data.toString());
+
+    if (event.Type === "container" && event.Action === "start") {
+      console.log(`Container ${event.Actor.Attributes.name} has started`);
+      const container = docker.getContainer(event.id);
+
+      const containerInfo = await container.inspect();
+      const containerName = containerInfo.Name.substring(1);
+      const ipAddress = containerInfo.NetworkSettings.IPAddress;
+
+      const exposedPorts = Object.keys(containerInfo.Config.ExposedPorts);
+
+      if (!exposedPorts && !exposedPorts.length) {
+        console.log("[Error] No exposed ports found");
+        return;
+      }
+
+      console.log(exposedPorts);
+      
+
+      const [port, type] = exposedPorts[0].split("/");
+      console.log(`Exposed port: ${port} (${type})`);
+      
+
+      if (type !== "tcp") {
+        console.log("[Error] Only TCP ports are supported but got", type);
+        return;
+      }
+
+      console.log(
+        `Registering ${containerName}.localhost --> ${ipAddress}:${port}`
+      );
+
+      db.set(containerName, {
+        ipAddress,
+        port,
+      });
+    }
+  });
+
+  stream.on("end", () => {
+    console.log("Docker event stream ended");
+  });
+});
+
+// Reverse Proxy - This server is responsible for routing the requests to the correct container
+const reverseProxyPort = 80;
+const reverseProxyApp = express();
+
+reverseProxyApp.use((req, res) => {
+  const hostname = req.hostname;
+  const subDomain = hostname.split(".")[0];
+
+  if (!db.has(subDomain)) {
+    return res.status(404).send("Not found");
+  }
+
+  const { ipAddress, port } = db.get(subDomain);
+
+  const target = `http://${ipAddress}:${port}`;
+
+  console.log(`Forwarding ${hostname} --> ${target}`);
+
+  return proxy.web(req, res, { target, changeOrigin: true });
+});
+
+const reverseProxy = http.createServer(reverseProxyApp);
+
+reverseProxy.listen(reverseProxyPort, () => {
+  console.log(`Reverse proxy listening on port ${reverseProxyPort}`);
+});
+
+// Management API - This API is responsible for managing the containers
 const managementAPI = express();
 managementAPI.use(express.json());
 
@@ -22,7 +108,7 @@ managementAPI.post("/run", async (req, res) => {
   if (!imageAlreadyExists) {
     console.log(`Pulling ${image}:${tag}`);
     await docker.pull(`${image}:${tag}`);
-  }else{
+  } else {
     console.log(`Image ${image}:${tag} already exists`);
   }
 
@@ -36,10 +122,13 @@ managementAPI.post("/run", async (req, res) => {
 
   await container.start();
 
+  const containerName = (await container.inspect()).Name.substring(1);
+
   return res.json({
     message: "Container started",
-    container: (await container.inspect()).Name,
+    container: containerName,
     image: `${image}:${tag}`,
+    url: `http://${containerName}.localhost:80/`,
   });
 });
 
